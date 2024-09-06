93e207c1be05b07bfcdb3a1859a372e15250fe48
Gennaro Tortone <gennaro.tortone@na.infn.it>
2024-09-06 14:25:35 +0200

    image-builder: restore ToolDAQ build

--- zynq/image-builder/scripts/install-tooldaq.sh
+++ zynq/image-builder/scripts/install-tooldaq.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 cd /opt
-git clone https://github.com/ToolFramework/ToolFrameworkCore.git
+git clone --branch v2.3.0 https://github.com/ToolFramework/ToolFrameworkCore.git
 cd ToolFrameworkCore
 make clean
 make -j4 
 
 cd /opt
-git clone https://github.com/ToolDAQ/ToolDAQFramework.git
+git clone --branch v2.3.0 https://github.com/ToolDAQ/ToolDAQFramework.git
 cd ToolDAQFramework
 make clean
 make -j4
